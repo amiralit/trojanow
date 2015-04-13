389780bcd663cfd14d14a9cf56b96a1f543bfff3
Amir A Tahmasebi <amirali.t@gmail.com>
2015-04-12 18:43:29 -0700

    added the first servlet

---
+++ trojanow-web/src/main/java/edu/trojanow/trojanowweb/Profile.java
@@ -0,0 +1,49 @@
+package edu.trojanow.trojanowweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.trojanow.trojandataaccesss.ProfileDao;
+import edu.trojanow.trojandataaccesss.ProfileDaoImpl;
+
+/**
+ * Servlet implementation class Profile
+ */
+public class Profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private ProfileDao mProfileDao = new ProfileDaoImpl();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+        out.println( "SimpleServlet Executed" );
+        out.flush();
+        out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final String myUsername = request.getParameter("username");
+		final String myPassword = request.getParameter("password");
+		
+		mProfileDao.insert(new edu.trojanow.trojanowmodel.Profile(myUsername, myPassword));
+	}
+
+}
